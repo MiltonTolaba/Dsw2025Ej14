ce56610a5bb9e73dd628f7df8918e0e626c62898
Milton <miltontolaba84@gmail.com>
2025-06-10 15:27:45 -0300

    avance

---
+++ Dsw2025Ej14.Api/Controller/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Dsw2025Ej14.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+namespace Dsw2025Ej14.Api.Controller
+{
+    [ApiController]
+    [Route("api")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly PersistenciaEnMemoria _persistencia;
+        public ProductsController(PersistenciaEnMemoria persistencia)
+        {
+            _persistencia = persistencia;
+        }
+
+        [HttpGet("products")]
+        public IActionResult GetAllActive()
+        {
+            var productosActivos = _persistencia
+                .ObtenerTodos()
+                .Where(p => p.IsActive)
+                .ToList();
+
+            if (productosActivos.Count == 0)
+                return NoContent();
+
+            return Ok(productosActivos);
+        }
+
+        [HttpGet("products/{sku}")]
+    }
+}
---
+++ Dsw2025Ej14.Api/Data/PersistenciaEnMemoria.cs
@@ -0,0 +1,26 @@
+﻿using Dsw2025Ej14.Api.Domain;
+using System.Text.Json;
+
+namespace Dsw2025Ej14.Api.Data
+{
+    public class PersistenciaEnMemoria
+    {
+        private List<Product> _productos;
+
+        public PersistenciaEnMemoria()
+        {
+            _productos = new List<Product>();
+            LoadProducts();
+        }
+        public List<Product> ObtenerTodos() => _productos;
+
+        public void LoadProducts()
+        {
+            string json = File.ReadAllText("products.json");
+            _productos = JsonSerializer.Deserialize<List<Product>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            }) ?? [];
+            }
+        }
+    }
